1ad1122b81dfe15fe0674943a8746435302ecc93
om mhaske <ommhaske1224@gmail.com>
2025-01-06 16:45:24 +0530

    Add sign-in page component with basic UI

---
+++ src/app/(auth)/sign-in/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <h1>Hello testing</h1>
+      <button className='bg-orange-500 border-spacing-1' >signIn</button>
+    </div>
+  )
+}
+
+export default page
