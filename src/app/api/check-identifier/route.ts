216610f33965debfb22cb3f8599cb837b75fc7dd
om mhaske <ommhaske1224@gmail.com>
2025-01-28 21:33:45 +0530

    Fix identifier API route

--- src/app/api/check-identifier/route.ts
+++ src/app/api/check-identifier/route.ts
@@ -18,7 +18,7 @@ export async function GET(request: Request) {
         { status: 400 }
       );
     }
-
+    
     const user = await UserModel.findOne({
       $or: [{ username: identifier }, { email: identifier }],
     });
