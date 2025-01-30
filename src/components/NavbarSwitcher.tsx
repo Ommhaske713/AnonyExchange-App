a0d30f8fdeb38c805c87d86274d23267d3b0bab9
om mhaske <ommhaske1224@gmail.com>
2025-01-30 11:55:45 +0530

    Add HomeNavbar component to NavbarSwitc

--- src/components/NavbarSwitcher.tsx
+++ src/components/NavbarSwitcher.tsx
@@ -4,10 +4,16 @@ import React from 'react';
 import { usePathname } from 'next/navigation';
 import Navbar from './Navbar';
 import DashboardNavbar from './DashboardNavbar';
+import HomeNavbar from './HomeNavbar';
 
 const NavbarSwitcher = () => {
   const pathname = usePathname();
   const isDashboard = pathname.startsWith('/dashboard');
+  const isHome = pathname === '/';
+
+  if (isHome) {
+    return <HomeNavbar />;
+  }
 
   return isDashboard ? <DashboardNavbar /> : <Navbar />;
 };
