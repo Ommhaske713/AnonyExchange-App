a637a05d6fce62d8f8133e4f3d70c48b4b0b783f
om mhaske <ommhaske1224@gmail.com>
2025-02-16 22:52:54 +0530

    bug fixed

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,7 +1,8 @@
 import DashboardNavbar from "@/components/DashboardNavbar";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-
+import  AuthProvider  from "@/context/AuthProvider";
+import { Toaster } from "@/components/ui/toaster"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
