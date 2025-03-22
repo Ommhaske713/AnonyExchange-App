7cf57f607d25c6629bdf8e7e6a9e1dd5598c73c7
om mhaske <ommhaske1224@gmail.com>
2025-03-22 09:05:24 +0530

    Updated middleware configuration

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,15 +3,13 @@ import { getToken } from 'next-auth/jwt';
 export { default } from 'next-auth/middleware';
 
 export const config = {
-  matcher: ['/dashboard/:path*', '/sign-in', '/sign-up', '/', '/verify/:path*'],
+  matcher: ['/dashboard/:path*', '/sign-in', '/sign-up', '/', '/verify/:path*','/((?!api|_next/static|_next/image|favicon.ico).*)'],
 };
 
 export async function middleware(request: NextRequest) {
   const token = await getToken({ req: request });
   const url = request.nextUrl;
 
-  // Redirect to dashboard if the user is already authenticated
-  // and trying to access sign-in, sign-up, or home page
   if (
     token &&
     (url.pathname.startsWith('/sign-in') ||
